53f16b139372bd42d73d2e6b2b753b25e49840ef
Michael Tigue <mtigue@web4uonline.com>
2013-08-15 23:38:23 -0400

    7

--- _vti_cnf/readinglink.js
+++ _vti_cnf/readinglink.js
@@ -1,5 +1,5 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|14 Aug 2013 04:54:32 -0000
+vti_timelastmodified:TR|16 Aug 2013 03:19:49 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_author:SR|web4u\\mtigue
 vti_modifiedby:SR|web4u\\mtigue
@@ -7,6 +7,6 @@ vti_timecreated:TR|12 Aug 2013 15:46:41 -0000
 vti_backlinkinfo:VX|rak.html rak2.html main-backup.html 26/main.html main2.html
 vti_syncwith_68.178.246.6\:21/yalecc:TX|14 Aug 2013 04:54:32 -0000
 vti_syncofs_68.178.246.6\:21/yalecc:TW|14 Aug 2013 04:55:14 -0000
-vti_nexttolasttimemodified:TW|12 Aug 2013 15:46:41 -0000
+vti_nexttolasttimemodified:TW|14 Aug 2013 04:54:32 -0000
 vti_cacheddtm:TX|14 Aug 2013 04:54:32 -0000
 vti_filesize:IR|754
