7356e45a615d1a74f421d24b14270897867ebe30
Michael Tigue <mtigue@web4uonline.com>
2013-08-15 21:07:58 -0400

    6

--- _vti_cnf/inc-header.js
+++ _vti_cnf/inc-header.js
@@ -6,6 +6,6 @@ vti_modifiedby:SR|web4u\\mtigue
 vti_timecreated:TR|12 Aug 2013 18:42:12 -0000
 vti_cacheddtm:TX|12 Aug 2013 18:42:12 -0000
 vti_filesize:IR|565
-vti_backlinkinfo:VX|26/main.html
+vti_backlinkinfo:VX|rak.html rak2.html main-backup.html 26/main.html main2.html
 vti_syncwith_68.178.246.6\:21/yalecc:TX|12 Aug 2013 18:42:12 -0000
 vti_syncofs_68.178.246.6\:21/yalecc:TW|13 Aug 2013 03:17:36 -0000
--- _vti_cnf/readinglink.js
+++ _vti_cnf/readinglink.js
@@ -4,7 +4,7 @@ vti_extenderversion:SR|12.0.0.0
 vti_author:SR|web4u\\mtigue
 vti_modifiedby:SR|web4u\\mtigue
 vti_timecreated:TR|12 Aug 2013 15:46:41 -0000
-vti_backlinkinfo:VX|26/main.html
+vti_backlinkinfo:VX|rak.html rak2.html main-backup.html 26/main.html main2.html
 vti_syncwith_68.178.246.6\:21/yalecc:TX|14 Aug 2013 04:54:32 -0000
 vti_syncofs_68.178.246.6\:21/yalecc:TW|14 Aug 2013 04:55:14 -0000
 vti_nexttolasttimemodified:TW|12 Aug 2013 15:46:41 -0000
